ce73fbb63df7c8f3e981c849cca573b52bec0564
Ben Root <ben.v.root@gmail.com>
2011-05-06 16:39:13 -0500

    Significant cleanup and (hopefully) speedup of ClusterMap.  Now uses BRadar as well. The ClusterMap no longer uses the white outline, though.

--- ClusterMap.py
+++ ClusterMap.py
@@ -1,8 +1,9 @@
 import numpy
 import matplotlib.pyplot as pyplot
-import inpoly
+from matplotlib import transforms
 
-from RadarPlotUtils import MakeReflectPPI
+from BRadar.plotutils import MakeReflectPPI
+#from RadarPlotUtils import MakeReflectPPI
 
 def ClusterMap(clusters, vals, indicesToShow,
 	       radarBG_alpha=0.15, dimmerBox_alpha=0.25,
@@ -42,7 +43,15 @@ def ClusterMap(clusters, vals, indicesToShow,
 
     (lons, lats) = numpy.meshgrid(clusters['lonAxis'], clusters['latAxis'])
     
-    domainMask = (lats < boxBoundy[0]) | (lats > boxBoundy[1]) | (lons < boxBoundx[0]) | (lons > boxBoundx[1]) | numpy.isnan(vals)
+    domainMask = (lats < boxBoundy[0]) | (lats > boxBoundy[1]) | \
+                 (lons < boxBoundx[0]) | (lons > boxBoundx[1]) | \
+                 numpy.isnan(vals)
+
+
+    # Create a transform object for the drop shadow.
+    dx, dy = 2/72., -2/72.
+    #white_offset = axis.transData + transforms.ScaledTranslation(dx, dy, axis.figure.dpi_scale_trans)
+    black_offset = axis.transData + transforms.ScaledTranslation(dx, dy, axis.figure.dpi_scale_trans)
 
 
     # elementCount determines how many things are going to be displayed in this
@@ -52,7 +61,7 @@ def ClusterMap(clusters, vals, indicesToShow,
     #    Then there is the possible radar background.
     #    Lastly, there is the possible dimmer box.
     # Note: this can be an over-estimate many clusters might be outside the bounding box.
-    elementCount = (3*len(indicesToShow)) + (radarBG_alpha > 0.0) + (dimmerBox_alpha > 0.0)
+    elementCount = (2*len(indicesToShow)) + (radarBG_alpha > 0.0) + (dimmerBox_alpha > 0.0)
 
     # Create an array of zorder values to use for the components of the ClusterMap.
     zorders = numpy.linspace(zorder, zorder + 1, num = elementCount, endpoint=False)
@@ -60,9 +69,10 @@ def ClusterMap(clusters, vals, indicesToShow,
     zorderIndex = 0
 
     #  ------ Radar Background --------------
+    # Don't bother plotting the BG if alpha is zero.
     if radarBG_alpha > 0.0 :
         MakeReflectPPI(vals, lats, lons, mask=domainMask,
-        	       colorbar=False, axis_labels=False, axis=axis,
+        	       colorbar=False, axis_labels=False, ax=axis,
 		       zorder=zorders[zorderIndex], alpha=radarBG_alpha,
 		       **kwargs)
         zorderIndex += 1
@@ -70,6 +80,7 @@ def ClusterMap(clusters, vals, indicesToShow,
 
 
     # --------Dimmer box--------
+    # Don't bother plotting the dimmer box if it is zero.
     if dimmerBox_alpha > 0.0 :
         axis.fill(bbBoxX, bbBoxY,
     	          'black', zorder = zorders[zorderIndex], alpha=dimmerBox_alpha)
@@ -97,30 +108,31 @@ def ClusterMap(clusters, vals, indicesToShow,
     for index, goodMembers in enumerate(clustMembers) :
         # If the cluster does not exist within the bounding box, don't bother rendering it.
         if not numpy.any(numpy.logical_not(domainMask[clusters['members_LatLoc'][goodMembers],
-				           	      clusters['members_LonLoc'][goodMembers]])) :
+                                                      clusters['members_LonLoc'][goodMembers]])) :
             continue
 
         clustMask.fill(True)
 
-	# Finding all members of the cluster
+        # Finding all members of the cluster
         clustMask[clusters['members_LatLoc'][goodMembers],
-		  clusters['members_LonLoc'][goodMembers]] = False
+                  clusters['members_LonLoc'][goodMembers]] = False
 
-	# Plotting the cluster (white line, black line, then reflectivity patch)
-	# This could probably be improved
-        # white highlight
-	outline = axis.contour(lons, lats, clustMask, 
-			       [0, 1], colors='w', linewidths=7.0, zorder = zorders[zorderIndex])
-        SetContourZorder(outline, zorders[zorderIndex])
-
+        # Plotting the cluster (white line, black line, then reflectivity patch)
+        # This could probably be improved
         # black outline
-	outline = axis.contour(lons, lats, clustMask, 
-		       	       [0, 1], colors='k', linewidths=4.5, zorder = zorders[zorderIndex + 1])
+        outline = axis.contour(lons, lats, clustMask,
+                               [0, 1], colors='k', linewidths=2.5, zorder = zorders[zorderIndex + 0])
         SetContourZorder(outline, zorders[zorderIndex + 1])
+        SetContourTransform(outline, black_offset)
 
-        MakeReflectPPI(vals, lats, lons, mask=clustMask, axis=axis, zorder=zorders[zorderIndex + 2],
-		               axis_labels=False, colorbar=False, **kwargs)
-        zorderIndex += 3
+        # white highlight... save time by re-using the contour data.
+        #for aPath in outline.collections[0].get_paths() :
+        #    axis.plot(*aPath.vertices.T, c='k', lw=2.5, zorder=zorders[zorderIndex],
+        #              transform=white_offset)
+        
+        MakeReflectPPI(vals, lats, lons, mask=clustMask, ax=axis, zorder=zorders[zorderIndex + 1],
+                       axis_labels=False, colorbar=False, **kwargs)
+        zorderIndex += 2
     
 
     # Return axis to whatever hold status it had before.
@@ -135,4 +147,8 @@ def SetContourZorder(theContours, zorder) :
     """
     for col in theContours.collections :
         col.set_zorder(zorder)
+
+def SetContourTransform(theContours, trans) :
+    for col in theContours.collections :
+        col.set_transform(trans)
 
