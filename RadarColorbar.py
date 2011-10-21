7721c78117a04fc5c4fd4b207f355bff8cb4d117
Ben Root <ben.v.root@gmail.com>
2011-10-21 11:26:43 -0500

    Updated the RadarColorbar.py script.  Doesn't save anymore.

--- RadarColorbar.py
+++ RadarColorbar.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python
-from RadarPlotUtils import ReflectInfo
+from BRadar.plotutils import NWS_Reflect
 
 
 '''
 Make a colorbar as a separate figure.
 '''
 
-from matplotlib import pyplot, mpl
+#from matplotlib import pyplot, mpl
+import matplotlib.pyplot as plt
+from matplotlib.colorbar import ColorbarBase
 
 # Make a figure and axes with dimensions as desired.
-fig = pyplot.figure()
+fig = plt.figure()
 ax1 = fig.add_axes([0.45, 0.05, 0.03, 0.75])
 
-# Set the colormap and norm to correspond to the data for which
-# the colorbar will be used.
-colorInfo = ReflectInfo()
 
 # ColorbarBase derives from ScalarMappable and puts a colorbar
 # in a specified axes, so it has everything needed for a
 # standalone colorbar.  There are many more kwargs, but the
 # following gives a basic continuous colorbar with ticks
 # and labels.
-cb1 = mpl.colorbar.ColorbarBase(ax1, cmap=colorInfo['ref_table'],
-                                   norm=colorInfo['norm'])
+cb1 = ColorbarBase(ax1, cmap=NWS_Reflect['ref_table'],
+                                   norm=NWS_Reflect['norm'])
 cb1.set_label('Reflectivity [dBZ]')
 
-pyplot.savefig('../../Documents/SPA/Colorbar_Raw.eps')
-pyplot.savefig('../../Documents/SPA/Colorbar_Raw.png', dpi=250)
+#pyplot.savefig('../../Documents/SPA/Colorbar_Raw.eps')
+#pyplot.savefig('../../Documents/SPA/Colorbar_Raw.png', dpi=250)
 
-
+plt.show()
