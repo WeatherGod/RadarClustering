2dce2556b10894ebe7cf4a13d6301a7fcc34ffe3
Ben Root <ben.v.root@gmail.com>
2011-10-25 20:09:31 -0500

    Clust2Corners now optionally saves a polygon pickle.

--- Clust2Corners.py
+++ Clust2Corners.py
@@ -14,126 +14,179 @@ import ZigZag.TrackFileUtils as TrackFileUtils
 import ZigZag.TrackUtils as TrackUtils
 import ZigZag.ParamUtils as ParamUtils
 
-parser = argparse.ArgumentParser(description="Convert a cluster file into a corner file")
-parser.add_argument("-r", "--run", dest = "runName",
-		  help = "Generate corner files for RUNNAME", metavar="RUNNAME")
-parser.add_argument("-p", "--path", dest="pathName",
-		  help = "PATHNAME for cluster files and radar data. Default: %(default)s",
-          metavar="PATHNAME", default=".")
 
-args = parser.parse_args()
+def _get_contour(xs, ys, feature) :
+    from matplotlib._cntr import Cntr
+    from itertools import chain
+    c = Cntr(xs, ys, feature, None)
+    # Returns a list where the first half of the elements are the boundaries
+    # The second half contain connection codes that can be ignored here.
+    # Can not assume clockwise or counter-clockwise traversal of the polygon
+    res = c.trace(0)
 
-if args.runName is None :
-    parser.error("Missing RUNNAME")
+    if len(res) == 0 :
+        return np.empty((0, 2), dtype='i')
+    else :
+        # Link up the contours with a nan between them
+        return np.vstack(chain(*[(ares, [(np.nan, np.nan)]) for ares in
+                                 res[:len(res)//2]]))
 
-runLoc = os.path.join(args.pathName, 'ClustInfo', args.runName)
-fileList = glob.glob(os.path.join(runLoc, '*.nc'))
-print "Run Location:", runLoc
-if (len(fileList) == 0) : print "WARNING: No files found for run '" + args.runName + "'!"
-fileList.sort()
+if __name__ == '__main__' :
+    parser = argparse.ArgumentParser(
+                        description="Convert a cluster file into a corner file")
+    parser.add_argument("-r", "--run", dest="runName",
+              help="Generate corner files for RUNNAME", metavar="RUNNAME")
+    parser.add_argument("-p", "--path", dest="pathName",
+              help=("PATHNAME for cluster files and radar data."
+                    " Default: %(default)s"),
+              metavar="PATHNAME", default=".")
+    parser.add_argument("-c", "--contours", dest='savePolys',
+                        help="Save polygons in a pickle called polygons.foo",
+                        action='store_true', default=False)
 
 
-volume_data = []
-cornerID = 0
-startTime = None
+    args = parser.parse_args()
 
-for frameNum, filename in enumerate(fileList) :
-    (clustParams, clusters) = LoadClustFile(filename)
+    if args.runName is None :
+        parser.error("Missing RUNNAME")
 
-    # Get rasterized reflectivity data
-    radData = LoadRastRadar(os.path.join(args.pathName,
-                                         clustParams['dataSource']))
-    radarName = radData['station']
-    if radarName == 'NWRT' :
-        radarName = 'PAR'
+    runLoc = os.path.join(args.pathName, 'ClustInfo', args.runName)
+    fileList = glob.glob(os.path.join(runLoc, '*.nc'))
+    print "Run Location:", runLoc
+    if (len(fileList) == 0) :
+         print "WARNING: No files found for run '" + args.runName + "'!"
+    fileList.sort()
 
-    rastData = np.squeeze(radData['vals'])
-    # Get site info
-    radarSite = radar.ByName(radarName, radar.Sites)[0]
-    # Get cluster info
-    clustMembers = [np.nonzero(clusters['clusterIndicies'] == clustIndx) for
-                    clustIndx in np.unique(clusters['clusterIndicies'])]
+    if args.savePolys :
+        from cPickle import dump
+        # Dictionary containing (cornerID, x-y vertices) key-value pairs
+        polygons = {}
 
-    if startTime is None :
-        startTime = clustParams['scantime']
 
-    xCentroids = []
-    yCentroids = []
-    sizeCentroids = []
-    idCentroids = []
+    volume_data = []
+    cornerID = 0
+    startTime = None
 
-    # Now, we gotta merge this information...
-    for aClust in clustMembers :
-        # produce a Nx2 array
-        locs = np.array([(clusters['members_LatLoc'][goodIndx],
-                          clusters['members_LonLoc'][goodIndx]) for goodIndx in aClust])
+    for frameNum, filename in enumerate(fileList) :
+        (clustParams, clusters) = LoadClustFile(filename)
+
+        # Get rasterized reflectivity data
+        radData = LoadRastRadar(os.path.join(args.pathName,
+                                             clustParams['dataSource']))
+        radarName = radData['station']
+        if radarName == 'NWRT' :
+            radarName = 'PAR'
+
+        rastData = np.squeeze(radData['vals'])
+        # Get site info
+        radarSite = radar.ByName(radarName, radar.Sites)[0]
+        # Get cluster info
+        clustMembers = [np.nonzero(clusters['clusterIndicies'] == clustIndx) for
+                        clustIndx in np.unique(clusters['clusterIndicies'])]
+
+        if startTime is None :
+            startTime = clustParams['scantime']
+
+        xCentroids = []
+        yCentroids = []
+        sizeCentroids = []
+        idCentroids = []
+
+        # For the polygons
+        clustMask = np.empty(rastData.shape, dtype=bool)
 
         # We have everything in Lat/Lon... we need to convert to cartesian
-        gridLons, gridLats = np.meshgrid(clusters['lonAxis'], clusters['latAxis'])
-        gridx, gridy = LonLat2Cart(radarSite['LON'], radarSite['LAT'],
+        gridLons, gridLats = np.meshgrid(clusters['lonAxis'],
+                                         clusters['latAxis'])
+        gridx, gridy = LonLat2Cart(radarSite['LON'],
+                                   radarSite['LAT'],
                                    gridLons, gridLats)
 
         dxdj, dxdi = np.gradient(gridx)
         dydj, dydi = np.gradient(gridy)
 
-        locIndices = zip(*locs)
+        # Now, we gotta merge this information...
+        for aClust in clustMembers :
+            # produce a Nx2 array
+            locs = np.array([(clusters['members_LatLoc'][goodIndx],
+                              clusters['members_LonLoc'][goodIndx]) for
+                             goodIndx in aClust])
 
-        yLocs = gridy[locIndices]
-        xLocs = gridx[locIndices]
+            if args.savePolys :
+                # Reset the mask
+                clustMask.fill(False)
 
+                clustMask[clusters['members_LatLoc'][aClust],
+                          clusters['members_LonLoc'][aClust]] = True
 
-        # Find the Center of Mass.
-        clustVals = rastData[locIndices]
-        yCentroids.append(np.average(yLocs, weights=clustVals))
-        xCentroids.append(np.average(xLocs, weights=clustVals))
-
-        # Find the size of the feature.
-        # This method assumes that the voxels are rhombus-shaped.
-        # TODO: A better assumption would be parallelogram-shaped
-        a = np.sqrt(dxdj[locIndices]**2 + dydj[locIndices]**2)
-        b = np.sqrt(dxdi[locIndices]**2 + dydi[locIndices]**2)
-        dA = a * b
-        sizeCentroids.append(np.sum(dA))
-
-        # Supply this centroid's unique feature ID
-        idCentroids.append(cornerID)
-        cornerID += 1
-
-    # Starting with initializing the volume object.
-    aVol = {'volTime': (clustParams['scantime'] - startTime) / 60.0,
-            'frameNum': frameNum,
-            'stormCells': np.array(zip(xCentroids, yCentroids, sizeCentroids, idCentroids),
-                                      dtype=TrackUtils.corner_dtype)}
-
-    # Then build up the volume info
-    volume_data.append(aVol)
+                verts = _get_contour(gridx, gridy, clustMask)
+                polygons[cornerID] = verts
 
 
 
-if len(volume_data) != 0 :
+            locIndices = zip(*locs)
 
-    simParams = dict()
-    simParams.update(ParamUtils.simDefaults)
-    simParams.update(ParamUtils.trackerDefaults)
+            yLocs = gridy[locIndices]
+            xLocs = gridx[locIndices]
 
-    xLims, yLims, tLims, frameLims = TrackUtils.DomainFromVolumes(volume_data)
 
-    simParams['xLims'] = xLims
-    simParams['yLims'] = yLims
-    simParams['tLims'] = tLims
-    simParams['frameCnt'] = len(volume_data)
+            # Find the Center of Mass.
+            clustVals = rastData[locIndices]
+            yCentroids.append(np.average(yLocs, weights=clustVals))
+            xCentroids.append(np.average(xLocs, weights=clustVals))
 
-    # These parameters are irrelevant.
-    simParams.pop('seed')
-    simParams.pop('totalTracks')
-    simParams.pop('endTrackProb')
-    simParams.pop('simConfFile')
-    simParams.pop('analysis_stem')
+            # Find the size of the feature.
+            # This method assumes that the voxels are rhombus-shaped.
+            # TODO: A better assumption would be parallelogram-shaped
+            a = np.hypot(dxdj[locIndices], dydj[locIndices])
+            b = np.hypot(dxdi[locIndices], dydi[locIndices])
+            dA = a * b
+            sizeCentroids.append(np.sum(dA))
 
-    simParams['simName'] = args.runName
-    TrackFileUtils.SaveCorners(os.path.join(runLoc, simParams['inputDataFile']),
-                               simParams['corner_file'],
-                               volume_data,
-                               path=runLoc)
-    ParamUtils.SaveConfigFile(os.path.join(runLoc, 'simParams.conf'), simParams)
+            # Supply this centroid's unique feature ID
+            idCentroids.append(cornerID)
+            cornerID += 1
+
+        # Starting with initializing the volume object.
+        aVol = {'volTime': (clustParams['scantime'] - startTime) / 60.0,
+                'frameNum': frameNum,
+                'stormCells': np.array(zip(xCentroids, yCentroids,
+                                           sizeCentroids, idCentroids),
+                                       dtype=TrackUtils.corner_dtype)}
+
+        # Then build up the volume info
+        volume_data.append(aVol)
+
+
+    if len(volume_data) != 0 :
+        simParams = dict()
+        simParams.update(ParamUtils.simDefaults)
+        simParams.update(ParamUtils.trackerDefaults)
+
+        (xLims, yLims, tLims,
+         frameLims) = TrackUtils.DomainFromVolumes(volume_data)
+
+        simParams['xLims'] = xLims
+        simParams['yLims'] = yLims
+        simParams['tLims'] = tLims
+        simParams['frameCnt'] = len(volume_data)
+
+        # These parameters are irrelevant.
+        simParams.pop('seed')
+        simParams.pop('totalTracks')
+        simParams.pop('endTrackProb')
+        simParams.pop('simConfFile')
+        simParams.pop('analysis_stem')
+
+        simParams['simName'] = args.runName
+        TrackFileUtils.SaveCorners(os.path.join(runLoc,
+                                                simParams['inputDataFile']),
+                                   simParams['corner_file'],
+                                   volume_data,
+                                   path=runLoc)
+        ParamUtils.SaveConfigFile(os.path.join(runLoc, 'simParams.conf'),
+                                  simParams)
+
+        if args.savePolys :
+            dump(polygons, open(os.path.join(runLoc, 'polygons.foo'), 'wb'))
 
